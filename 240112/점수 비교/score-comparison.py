ca8a27ab968f32cc6c5faad4c3ba4b3c06fac0b7
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-01-12 16:50:54 +0900

    Title: 점수 비교, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240112/점수 비교/score-comparison.py
@@ -0,0 +1,11 @@
+mathA, mathB = input().split()
+mathA = int(mathA)
+mathB = int(mathB)
+engA, engB = input().split()
+engA = int(engA)
+engB = int(engB)
+
+if (mathA > mathB and engA > engB) :
+    print(1)
+else :
+    print(0)
