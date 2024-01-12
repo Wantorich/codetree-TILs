eaeedca16f4750d4b7521c7bf2ab9630bff4d686
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-01-12 16:41:05 +0900

    Title: 비교 연산, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240112/비교 연산/comparison-operator.py
@@ -0,0 +1,10 @@
+a, b = input().split()
+a = int(a)
+b = int(b)
+
+print('%d' % 1 if a >= b else 0)
+print('%d' % 1 if a > b else 0)
+print('%d' % 1 if b >= a else 0)
+print('%d' % 1 if b >= a else 0)
+print('%d' % 1 if a == b else 0)
+print('%d' % 1 if a != b else 0)
