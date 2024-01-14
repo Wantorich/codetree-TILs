8d24336540c015d78185cef506ccf00cd46bf0bf
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-01-14 15:02:39 +0900

    Title: 369 게임, Time: 153ms, Memory: 24MB, Status: Passed - CodeTree

--- 240114/369 게임/369-game.py
+++ 240114/369 게임/369-game.py
@@ -1,10 +1,8 @@
 n = int(input())
 for i in range(1, n+1) :
-    head = -1
-    if i > 10 :
-        head = i // 10
+    head = i // 10
     tail = i % 10
-    if i % 3 == 0 or head % 3 == 0 or tail % 3 == 0:
+    if i % 3 == 0 or (head and head % 3 == 0) or (tail and tail % 3 == 0):
         print(0, end=' ')
     else :
         print(i, end=' ')
