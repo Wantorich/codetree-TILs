b44a7717513fa2c13d66a9d5155b0f0dbe42e3e6
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-01-14 15:00:42 +0900

    Title: 369 게임, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240114/369 게임/369-game.py
+++ 240114/369 게임/369-game.py
@@ -1,11 +1,10 @@
 n = int(input())
 for i in range(1, n+1) :
-    sum_ = 0
+    head = -1
     if i > 10 :
-        sum_ = (i // 10) + (i % 10)
-    else :
-        sum_ = i
-    if i % 3 == 0 or sum_ % 3 == 0 :
+        head = i // 10
+    tail = i % 10
+    if i % 3 == 0 or head % 3 == 0 or tail % 3 == 0:
         print(0, end=' ')
     else :
         print(i, end=' ')
