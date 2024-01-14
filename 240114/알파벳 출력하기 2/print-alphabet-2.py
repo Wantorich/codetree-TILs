073c392c27bfa0bf4ac69534ee2d922ab20fa84b
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-01-14 16:32:00 +0900

    Title: 알파벳 출력하기 2, Time: 150ms, Memory: 24MB, Status: Passed - CodeTree

--- 240114/알파벳 출력하기 2/print-alphabet-2.py
+++ 240114/알파벳 출력하기 2/print-alphabet-2.py
@@ -7,7 +7,12 @@ for i in range(n) :
     for j in range(n) :
         if i <= j :
             print(char, end=' ')
-            char = chr(ord(char) + 1)
+            next_char = ord(char) + 1
+            if next_char > 90 :
+                next_char = 'A'
+            else :
+                next_char = chr(next_char) 
+            char = next_char
         else :
             print(' ', end=' ')
     print()
