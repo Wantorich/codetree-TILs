9bec7fe9d67d9b073e27681ce11c696b43c5d66c
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-01-14 22:35:34 +0900

    Title: 약수의 개수가 3개인 수, Time: 121ms, Memory: 25MB, Status: Passed - CodeTree

--- 240114/약수의 개수가 3개인 수/square-of-prime-number.py
+++ 240114/약수의 개수가 3개인 수/square-of-prime-number.py
@@ -3,7 +3,7 @@ cnt = 0
 for num in range(a, b+1) :
     sub_cnt = 0
     for i in range(1, num+1) :
-        if num % 1 == 0 :
+        if num % i == 0 :
             sub_cnt += 1
     if sub_cnt == 3 :
         cnt += 1
