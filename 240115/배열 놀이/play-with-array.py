86e84818458f8dfd90e73a692ce366f5570ab61d
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-01-15 19:02:32 +0900

    Title: 배열 놀이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240115/배열 놀이/play-with-array.py
@@ -0,0 +1,15 @@
+n, q = map(int, input().split())
+arr = list(map(int, input().split()))
+
+for _ in range(n) :
+    line = list(map(int, input().split()))
+    odr, idx = line[0], line[1]
+    if odr == 1 :
+        print(arr[idx-1])
+    elif odr == 2 :
+        if idx in arr :
+            print(arr.index(idx)+1)
+    elif odr == 3 :
+        endex = line[2]
+        for i in range(idx-1, endex) :
+            print(arr[i], end=' ')
