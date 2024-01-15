50e38be5f4ec91cdac89d55402db8eead17a83f8
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-01-15 23:52:05 +0900

    Title: n개의 숫자 중 최대 2개, Time: 240ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240115/n개의 숫자 중 최대 2개/two-max-of-n-num.py
+++ 240115/n개의 숫자 중 최대 2개/two-max-of-n-num.py
@@ -1,27 +1,14 @@
-import sys
-
 n = int(input())
 arr = list(map(int, input().split()))
+
 max_val = arr[0]
 idx = -1
-for num in arr[1:] :
-    if num >= max_val :
+for i, num in enumerate(arr) :
+    if num > max_val :
         max_val = num
-        idx = arr.index(num)
+        idx = i
 
-
-max_left = -sys.maxsize
-for num in arr[:idx] :
-    if idx <= 0 :
-        break;
-    if max_left <= num :
-        max_left = num
-
-max_right = -sys.maxsize
-for num in arr[idx+1:] :
-    if max_right <= num :
-        max_right = num
-
-# print(max_left, max_right)
-print(max_val, end=' ')
-print('%d' % max_left if max_left >= max_right else max_right)
+max_left = max(arr[:idx])
+max_right = max(arr[idx+1:])
+max_sec = max_left if max_left >= max_right else max_right
+print(max_val, max_sec)
