0c65a2c39d10d4d15fce73bbd2aa7e2b511a7a58
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-01-15 19:38:24 +0900

    Title: 연속부분수열일까, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240115/연속부분수열일까/contiguous-array-or-not.py
+++ 240115/연속부분수열일까/contiguous-array-or-not.py
@@ -6,16 +6,28 @@ arr_B = list(map(int, input().split()))
 # 모두 존재해야하고 인덱스도 연속되야함
 temp = -1
 flag = True
+
+if arr_B[0] in arr_A :
+    idx = arr_A.index(arr_B[0])
+    arr_A = arr_A[idx:] # 배열 슬라이싱
+else :
+    flag = False
+
 for num_B in arr_B : 
     if num_B in arr_A :
         idx = arr_A.index(num_B)
-        if temp == -1 :
-            temp = idx
-        else :
-            if idx - temp == 1 :
-                continue
-            else :
-                flag = False
-                break
+        if idx != 0 :
+            flag = False
+            break
+        arr_A = arr_A[idx+1:] # 배열 슬라이싱
+
+        # if temp == -1 :
+        #     temp = idx
+        # else :
+        #     if idx - temp == 1 :
+        #         continue
+        #     else :
+        #         flag = False
+        #         break
 
 print('%s' % 'Yes' if flag else 'No')
