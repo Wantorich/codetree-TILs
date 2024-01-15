9a1f146d75dfd014e7872fec20df66e674aa722a
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-01-15 23:58:59 +0900

    Title: 연속부분수열일까, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240115/연속부분수열일까/contiguous-array-or-not.py
+++ 240115/연속부분수열일까/contiguous-array-or-not.py
@@ -4,30 +4,12 @@ arr_B = list(map(int, input().split()))
 
 # B의 원소들을 각각 A에 있는지 확인하고
 # 모두 존재해야하고 인덱스도 연속되야함
-temp = -1
-flag = True
+# 문제는 2개이상 같은 원소가 존재할수있다는거야
+flag = False
 
-if arr_B[0] in arr_A :
-    idx = arr_A.index(arr_B[0])
-    arr_A = arr_A[idx:] # 배열 슬라이싱
-else :
-    flag = False
-
-for num_B in arr_B : 
-    if num_B in arr_A :
-        idx = arr_A.index(num_B)
-        if idx != 0 :
-            flag = False
-            break
-        arr_A = arr_A[idx+1:] # 배열 슬라이싱
-
-        # if temp == -1 :
-        #     temp = idx
-        # else :
-        #     if idx - temp == 1 :
-        #         continue
-        #     else :
-        #         flag = False
-        #         break
+len_diff = len_A - len_B
+for i in range(len_diff+1) :
+    if arr_A[i:i+len_diff] == arr_B :
+        flag = True
 
 print('%s' % 'Yes' if flag else 'No')
