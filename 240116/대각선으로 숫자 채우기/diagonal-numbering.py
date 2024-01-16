63cec47b3c16bbbf4a6fd41c6e29dd42d73f8f0a
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-01-16 22:04:47 +0900

    Title: 대각선으로 숫자 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240116/대각선으로 숫자 채우기/diagonal-numbering.py
+++ 240116/대각선으로 숫자 채우기/diagonal-numbering.py
@@ -9,23 +9,38 @@ arr_2d = [
 ]
 
 cnt = 1
-x = y = 0
-phase_x = 1
-phase_y = 0
-while x <= n-1 and y <= m-1 :
-    arr_2d[x][y] = cnt
-    cnt += 1
-    x += 1
-    y -= 1
-    if x >= n or y < 0 :
-        phase_y += 1
-        if phase_y <= m-1 :
-            x = 0
-            y = phase_y
-        else :
-            x = phase_x
-            phase_x += 1
-            y = m-1        
+for j in range(m) :
+    for i in range(j+1) :
+        arr_2d[i][j-i] = cnt
+        cnt += 1
+
+for i in range(1, n) :
+    k = 0
+    for j in range(m-1, -1, -1) :
+        if i+k >= n :
+            break
+        arr_2d[i+k][j] = cnt
+        k += 1
+        cnt += 1
+
+# cnt = 1
+# x = y = 0
+# phase_x = 1
+# phase_y = 0
+# while x <= n-1 and y <= m-1 :
+#     arr_2d[x][y] = cnt
+#     cnt += 1
+#     x += 1
+#     y -= 1
+#     if x >= n or y < 0 :
+#         phase_y += 1
+#         if phase_y <= m-1 :
+#             x = 0
+#             y = phase_y
+#         else :
+#             x = phase_x
+#             phase_x += 1
+#             y = m-1        
 
 for row in arr_2d :
     for num in row :
