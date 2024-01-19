d39377555cfc2dacd1fd20c0f7a1d511956a113d
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-01-19 14:25:11 +0900

    Title: 함수를 이용한 369 게임, Time: 200ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240119/함수를 이용한 369 게임/369-games-using-functions.py
+++ 240119/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,20 +1,30 @@
 a, b = map(int, input().split())
 
+def is_3_mulitple(n) :
+    str_num = str(n)
+    for s in str_num :
+        num = int(s)
+        if num % 3 == 0 :
+            return True
+    return False
+
 def is_magic_num(n) :
     # 3, 6, 9 중 하나가 들어가있거나 
     # 즉 모두 안들어있는경우를 전체에서 뺌 
     if n % 3 == 0 :
         return True
     else :
-        if n >= 10 : 
-            divisor = n // 10
-            remain = n % 10
-            if (divisor % 3 == 0) or (remain % 3 == 0) :
-                return True
-        else :
-            if n % 3 == 0 :
-                return True
-        return False
+        if is_3_mulitple(n) :
+            return True
+        # if n >= 10 : 
+        #     divisor = n // 10
+        #     remain = n % 10
+        #     if (divisor % 3 == 0) or (remain % 3 == 0) :
+        #         return True
+        # else :
+        #     if n % 3 == 0 :
+        #         return True
+    return False
         
 
 
