883051f973a6741fce4f5ed4beb441867a50dc32
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-01-19 15:45:36 +0900

    Title: 2개 이상의 알파벳, Time: 145ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240119/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -0,0 +1,16 @@
+def have_diff_chars(word) :
+    # alphabet number is 26
+    arr = [0] * 26
+    for c in word :
+        index = ord(c) - ord('a')
+        if arr[index] > 0 :
+            return True
+        else :
+            arr[index] += 1
+    return False
+
+word = input()
+if have_diff_chars(word) :
+    print('Yes')
+else :
+    print('No')
