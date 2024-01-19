a147cb6da189721f26c6a34dc5527a345df492bb
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-01-19 16:03:20 +0900

    Title: 두 정수에 대한 연산값 2, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 240119/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
+++ 240119/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -1,8 +1,14 @@
 def compute(arr) :
-    a = min(arr[0], arr[1])
-    b = max(arr[0], arr[1])
-    arr[0] = a + 10
-    arr[1] = b * 2
+    a = arr[0]
+    b = arr[1]
+    if a < b :
+        a += 10
+        b *= 2
+    else :
+        a *= 2 
+        b += 10
+    arr[0] = a
+    arr[1] = b
 
 arr = list(map(int, input().split()))
 compute(arr)
