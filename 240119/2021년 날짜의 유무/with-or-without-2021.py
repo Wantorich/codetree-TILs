68956dcc300155ea098326642b8666cdbc5fe15b
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-01-19 15:37:17 +0900

    Title: 2021년 날짜의 유무, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240119/2021년 날짜의 유무/with-or-without-2021.py
@@ -0,0 +1,27 @@
+m, d = map(int, input().split())
+# 4, 6, 9 ,11 -> 30days
+# 2 -> 28days
+
+def is_in_2021(m, d) :
+    if m == 2 :
+        if d > 28 :
+            return False
+        else :
+            return True
+    elif m == 4 or m == 6 or m == 9 or m == 11 :
+        if d > 31 :
+            return False
+        else :
+            return True
+    elif m <= 12 :
+        if d > 30 :
+            return False
+        else :
+            return True
+    else :
+        return False
+
+if is_in_2021(m, d) :
+    print('Yes')
+else :
+    print('No')
