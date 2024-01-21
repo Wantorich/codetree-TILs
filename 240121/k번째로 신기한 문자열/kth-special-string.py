01f9a93f6586259d4bebeaf36a4737a0aa72f872
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-01-21 23:39:19 +0900

    Title: k번째로 신기한 문자열, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240121/k번째로 신기한 문자열/kth-special-string.py
+++ 240121/k번째로 신기한 문자열/kth-special-string.py
@@ -6,7 +6,7 @@ words = [
 words.sort()
 cnt = 0
 for word in words :
-    if target in word :
+    if word.startswith(target) :
         cnt += 1
         if cnt == int(k) :
             print(word)
