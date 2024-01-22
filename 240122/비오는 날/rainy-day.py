f9642ae1f05e55f0d9623f5a9c6a6666fb149031
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-01-22 18:46:30 +0900

    Title: 비오는 날, Time: 143ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240122/비오는 날/rainy-day.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+class Weather :
+    def __init__(self, date, day, weather) :
+        self.date = date
+        self.day = day
+        self.weather = weather
+
+    def print_(self) :
+        print(self.date, self.day, self.weather, sep=' ')
+
+n = int(input())
+weathers = []
+date_diffs = []
+date_now = datetime.now()
+index = -1
+for i in range(n) :
+    date, day, weather = input().split()
+    if weather == 'Rain' :
+        y, m, d = map(int, date.split('-'))
+        date_cmp = datetime(y,m,d)
+        date_diffs.append(abs(date_now - date_cmp))
+        weathers.append(Weather(date,day,weather))
+
+index = date_diffs.index(min(date_diffs))
+weathers[index].print_()
