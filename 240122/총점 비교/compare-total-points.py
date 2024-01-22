b8a1149344bcde8563a79403572f826ba1279ba8
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-01-22 21:38:23 +0900

    Title: 총점 비교, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240122/총점 비교/compare-total-points.py
@@ -0,0 +1,9 @@
+n = int(input())
+people = [
+    tuple(input().split())
+    for _ in range(n)
+]
+people.sort(key=lambda x : (int(x[1])) + (int(x[2])) + (int(x[3])))
+
+for name, kor, eng, math in people :
+    print(name, kor, eng, math)
