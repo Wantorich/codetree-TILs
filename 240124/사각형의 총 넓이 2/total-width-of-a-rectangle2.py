8449025cd855160212ecbe65b929db9bc74a50ce
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-01-24 14:50:57 +0900

    Title: 사각형의 총 넓이 2, Time: 116ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240124/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
+++ 240124/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -6,9 +6,7 @@ rects = [
 
 coord = [[0 for _ in range(201)] for _ in range(201)]
 for rect in rects :
-    min_ = min(rect)
-    if min_ < 0 :
-        rect = map(lambda x : x + min_, rect)
+    rect = map(lambda x : x + 100, rect)
     x1, y1, x2, y2 = rect
     for row in range(y1, y2) :
         for col in range(x1, x2) :
