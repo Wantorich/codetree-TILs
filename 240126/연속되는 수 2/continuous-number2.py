fd43d68def2e1c18b3df63400c21c8d06a4909ce
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-01-26 13:55:56 +0900

    Title: 연속되는 수 2, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240126/연속되는 수 2/continuous-number2.py
+++ 240126/연속되는 수 2/continuous-number2.py
@@ -1,11 +1,16 @@
 n = int(input())
 cnt = 1
+max_cnt = cnt
 prev = int(input())
 while n > 1 :
     num = int(input())
     if num != prev :
-        cnt += 1
+        cnt = 1
         prev = num
+    else :
+        cnt += 1
+    if max_cnt < cnt :
+        max_cnt = cnt
     n -= 1
 
-print(cnt)
+print(max_cnt)
