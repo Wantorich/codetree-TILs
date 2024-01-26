8dcfa18136d5edf9281896df3eb5b1a397d8e9a8
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-01-26 14:50:45 +0900

    Title: 벌금은 누구에게, Time: 152ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240126/벌금은 누구에게/who-will-pay.py
+++ 240126/벌금은 누구에게/who-will-pay.py
@@ -1,10 +1,10 @@
 n, m, k = map(int, input().split())
-students = [0] * n
-penalty = [int(input())-1 for _ in range(m)]
+students = [0] * (n+1)
+penalty = [int(input()) for _ in range(m)]
 res = -1
 for index in penalty :
     students[index] += 1
     if students[index] >= k :
-        res = index + 1
+        res = index
 
 print(res)
