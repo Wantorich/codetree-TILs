79ef3d7a074b2c11c6aa47e630d349a572a5317f
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-01-29 17:44:36 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 228ms, Memory: 24MB, Status: Passed - CodeTree

--- 240129/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240129/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,14 +1,29 @@
 n, k, p, t = map(int, input().split())
-coders = [0] * (n+1)
+coders, count = [0] * (n+1), [0] * (n+1)
 coders[p] = 1
+count[p] = k
 
 info_arr = [tuple(map(int, input().split())) for _ in range(t)]
 info_arr.sort(key = lambda x : x[0])
 
-for info in info_arr[:k] :
+for info in info_arr :
     _, x, y = info
-    if coders[x] == 1 or coders[y] == 1 :
-        coders[x] = 1
+    if coders[x] != 1 and coders[y] != 1 :
+        continue
+    elif coders[x] == 1 and coders[y] != 1 :
+        if count[x] <= 0 :
+            continue
+        count[x] -= 1
         coders[y] = 1
+        count[y] = k
+    elif coders[x] != 1 and coders[y] == 1 :
+        if count[y] <= 0 :
+            continue
+        count[y] -= 1
+        coders[x] = 1
+        count[x] = k
+    else :
+        count[x] -= 1
+        count[y] -= 1
 
 print(''.join(map(str, coders[1:])))
