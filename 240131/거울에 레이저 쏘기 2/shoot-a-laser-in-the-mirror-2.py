0a30c15e355e7982b3fe5c9ceac530c922b918f6
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-01-31 15:14:49 +0900

    Title: 거울에 레이저 쏘기 2, Time: 397ms, Memory: 74MB, Status: Wrong Answer - CodeTree

--- 240131/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 240131/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -6,21 +6,29 @@ for i in range(1, n+1) :
     for j in range(1, n+1) :
         squares[i][j] = slash[j-1]
 
-k = int(input()) - 1
+k = int(input())
 dxs, dys = [1, 0, -1, 0], [0, -1, 0, 1] # S W N E
 
-# 0 1 2 3 -> S W N E
-dir_num = k // n # init dir
-# x, y -> 0 ~ n+1, 0 ~ n+1
-init = k % n + dir_num * n
-if dir_num == 0 :
-    x, y = 0, k+1
-elif dir_num == 1 :
-    x, y = init, n+1
-elif dir_num == 2 :
-    x, y = n+1, init
-else :
-    x, y = init, 0
+x, y = 0, 0
+circuit = 3
+for _ in range(k) :
+    nx, ny = x + dxs[circuit], y + dys[circuit]
+    if (nx == 0 and ny == n+1) or (nx == n+1 and ny == n+1) and (nx == n+1 and ny == 0) :
+        circuit = (circuit + 1) % 4
+        nx, ny = nx + dxs[circuit], ny + dys[circuit]
+    x, y = nx, ny
+    
+# 0 1 2 3 -(> S W N E
+dir_num = (k - 1) // n # init dir
+# init = k % n + dir_num * n
+# if dir_num == 0 :
+#     x, y = 0, k+1
+# elif dir_num == 1 :
+#     x, y = k % n + dir_num, n+1
+# elif dir_num == 2 :
+#     x, y = n+1, init
+# else :
+#     x, y = init, 0
 
 def in_range(x,y) :
     return 1 <= x <= n and 1 <= y <= n
