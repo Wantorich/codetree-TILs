c8f29c157e1c3ca878a1033f416350ee31e0ba22
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-02-03 15:28:57 +0900

    Title: 오목, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240203/오목/O-mok.py
@@ -0,0 +1,51 @@
+size = 19
+grid = [list(map(int, input().split())) for _ in range(size)]
+
+winner = 0
+x, y = -1, -1
+
+def in_range(i, j) :
+    global size
+    return 0 <= i < size and 0 <= j < size
+
+def is_win_horizon(i, j) :
+    curr = grid[i][j]
+    for k in range(4) :
+        j += 1
+        if in_range(i,j) and grid[i][j] != curr :
+            return False
+    return True
+
+def is_win_vertical(i, j) :
+    curr = grid[i][j]
+    for k in range(4) :
+        i += 1
+        if in_range(i,j) and grid[i][j] != curr :
+            return False
+    return True
+
+def is_win_cross(i, j) :
+    curr = grid[i][j]
+    for k in range(4) :
+        i, j = i+1, j+1
+        if in_range(i,j) and grid[i][j] != curr :
+            return False
+    return True
+
+
+
+for i in range(size) :
+    for j in range(size) :
+        if grid[i][j] != 0 :
+            if is_win_horizon(i, j) :
+                winner = grid[i][j]
+                x, y = i, j+2
+            if is_win_vertical(i, j) :
+                winner = grid[i][j]
+                x, y = i+2, j
+            if is_win_cross(i,j) :
+                winner = grid[i][j]
+                x, y = i+2, j+2
+
+print(winner)
+print(x+1, y+1)
