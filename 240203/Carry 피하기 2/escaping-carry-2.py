ec8b286ac5361a9cb19bec9e5c46d18d5e3d5d67
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-02-03 14:54:10 +0900

    Title: Carry 피하기 2, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240203/Carry 피하기 2/escaping-carry-2.py
+++ 240203/Carry 피하기 2/escaping-carry-2.py
@@ -6,12 +6,12 @@ nums = [int(input()) for _ in range(n)]
 def is_carry(x,y,z) :
     while x != 0 and y != 0 and z != 0:
         remain_add = (x % 10) + (y % 10) + (z % 10)
-        if remain_add > 10 :
+        if remain_add >= 10 :
             return True
         x, y, z = x // 10, y // 10, z // 10
     return False
 
-max_val = -sys.maxsize
+max_val = -1
 for i in range(n) :
     for j in range(i+1, n) :
         for k in range(j+1, n) :
