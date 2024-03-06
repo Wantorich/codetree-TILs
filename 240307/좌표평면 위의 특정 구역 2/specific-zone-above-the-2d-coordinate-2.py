c68bb12e8cab65b7c8b049bb413bfd3082b30840
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-03-07 07:48:35 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240307/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
@@ -0,0 +1,31 @@
+# 직사각형의 넓이를 최소로하려면
+# 점들 중에서 y값의 최소, 최대 구하고
+# x값의 최소 최대 구해서 넓이구하면됨
+
+n = int(input())
+segments = [tuple(map(int, input().split())) for _ in range(n)]
+
+ans = 10000
+for i in range(n):
+    # count = [0] * 11 # initialize
+    left, right = 40000, 1
+    top, bottom = 1, 40000
+    for j in range(n):
+        if j == i:
+            continue
+        x, y = segments[j]
+        left = min(left, x)
+        right = max(right, x)
+        top = max(top, y)
+        bottom = min(bottom, y)
+        # print(left, right, top, bottom)
+        # x1, x2 = segments[j]
+        # for k in range(x1, x2 + 1):
+        #     count[k] += 1
+
+    area = (right - left) * (top - bottom)
+    ans = min(ans, area)
+    # max_cnt = max(count)
+    # ans = min(ans, max_cnt)
+
+print(ans)
