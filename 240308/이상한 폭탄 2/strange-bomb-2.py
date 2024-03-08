2222c5a0141b6e7816580829b8b659fc5bc2bda4
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-03-08 23:25:49 +0900

    Title: 이상한 폭탄 2, Time: 108ms, Memory: 28MB, Status: Passed - CodeTree

--- 240308/이상한 폭탄 2/strange-bomb-2.py
+++ 240308/이상한 폭탄 2/strange-bomb-2.py
@@ -3,9 +3,9 @@ bomb_set = [int(input()) for _ in range(n)]
 
 max_bomb = -1
 for i in range(n) :
+    curr_bomb = bomb_set[i]
     for j in range(i+1, min(i+1+k, n)) :
-        curr_bomb = bomb_set[i]
         if curr_bomb == bomb_set[j] :
-            max_bomb = curr_bomb
+            max_bomb = max(max_bomb, curr_bomb)
 
 print(max_bomb)
