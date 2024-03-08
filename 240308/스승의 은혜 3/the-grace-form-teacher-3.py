47a20790ca5656f5c507d66fbf05f3665addf279
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-03-08 17:31:53 +0900

    Title: 스승의 은혜 3, Time: 120ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240308/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240308/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,14 +1,25 @@
 n, budget = map(int, input().split())
 data = [tuple(map(int, input().split())) for _ in range(n)]
-price = [data[i][0] + data[i][1] for i in range(n)]
-price.sort()
+
+# 가격 리스트를 만들어야함
+price_list = []
+for i in range(n) :
+    price_temp = []
+    for j in range(n) :
+        price, ship = data[j][0], data[j][1]
+        if j == i :
+            price //= 2
+        price_temp.append(price+ship)
+    price_temp.sort()
+    price_list.append(price_temp)
 
 max_pay = 0
 for i in range(n) :
+    curr_price = price_list[i]
     pay = budget
     cnt = 0
     for j in range(n) :
-        p = price[j]
+        p = curr_price[j]
         if i == j :
             p //= 2
         pay -= p
