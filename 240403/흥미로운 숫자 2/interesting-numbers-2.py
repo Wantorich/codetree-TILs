1e5e6ab854eb36c854cd6afd2ef6d5d2369fb906
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-04-03 00:23:51 +0900

    Title: 흥미로운 숫자 2, Time: 272ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240403/흥미로운 숫자 2/interesting-numbers-2.py
@@ -0,0 +1,24 @@
+X, Y = map(int, input().split())
+
+def find_num(n) :
+    str_nums = list(str(n))
+    str_nums.sort()
+    fst_ = str_nums[0]
+    last_ = str_nums[-1]
+    fst_cnt = last_cnt = 0
+    for num in str_nums :
+        if fst_ != num :
+            fst_cnt += 1
+        if last_ != num :
+            last_cnt += 1
+    
+    if fst_cnt == 1 or last_cnt == 1 :
+        return True
+    else :
+        return False
+
+ans = 0
+for i in range(X, Y+1) :
+    if find_num(i) :
+        ans += 1
+print(ans)
