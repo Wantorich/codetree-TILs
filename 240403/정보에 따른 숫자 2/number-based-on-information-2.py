d4664e8fd580549034a983725beec48873093de2
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-04-03 11:58:44 +0900

    Title: 정보에 따른 숫자 2, Time: 141ms, Memory: 25MB, Status: Passed - CodeTree

--- 240403/정보에 따른 숫자 2/number-based-on-information-2.py
+++ 240403/정보에 따른 숫자 2/number-based-on-information-2.py
@@ -1,21 +1,22 @@
 import sys
 
+MAX_SIZE = 1000
 def find(k) :
     min_s = min_n = sys.maxsize
-    for i in range(a, b+1) :
-        if places[i] > 0 :
-            min_s = min(min_s, abs(i - k))
-        elif places[i] < 0 :
-            min_n = min(min_n, abs(i - k))
+    for i in range((MAX_SIZE+1)) :
+        dis = abs(i-k)
+        if places[i] > 0 and min_s > dis:
+            min_s = dis
+        elif places[i] < 0 and min_n > dis:
+            min_n = dis
     if min_s <= min_n :
         return True
     else : 
         return False
             
-
 T, a, b = map(int, input().split())
 points = [tuple(input().split()) for _ in range(T)]
-places = [0] * 1001
+places = [0] * (MAX_SIZE+1)
 cnt = 0
 
 for (c, index) in points :
