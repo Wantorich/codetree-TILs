fbd8e79062c3107481b8b23e523eed470302dad7
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-04-04 00:24:19 +0900

    Title: 빙산의 일각 2, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240404/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -0,0 +1,14 @@
+n = int(input())
+heights = [int(input()) for _ in range(n)]
+ans = 0
+for i in range(n) :
+    temp = [height - i for height in heights]
+    cnt = 0
+    for j in range(n) :
+        if temp[j] > 0 :
+            cnt += 1
+            if j+1 < n and temp[j+1] > 0 :
+                cnt -= 1
+    ans = max(ans, cnt)
+
+print(ans)
