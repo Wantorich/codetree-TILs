8679cabbb80592f3de1ac9f09c81b3bfa5adefe4
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-04-12 11:20:45 +0900

    Title: 팀으로 하는 틱택토 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240412/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -0,0 +1,38 @@
+grid = []
+bingo = [
+    [0,1,2], [3,4,5], [6,7,8],
+    [0,3,6], [1,4,7], [2,5,8],
+    [0,4,8], [2,4,6]
+]
+
+def can_win(a, b) :
+    exist = [False] * 9
+    for i in range(9) :
+        if a == temp[i] or b == temp[i] :
+            exist[i] = True
+    for a, b, c in bingo :
+        if exist[a] == True and exist[b] == True and exist[c] == True :
+            return True
+    return False
+
+for i in range(3) :
+    input_ = list(map(int, input()))
+    grid.extend(input_)
+
+nums = set(grid)
+ans = 0
+
+for i in range(len(nums)) :
+    for j in range(i+1, len(nums)) :
+        temp = grid
+        if can_win(i, j) :
+            ans += 1
+
+print(ans)
+
+
+# 00 01 02, 10 11 12
+# 00 10 20, 01 11 21
+# 00 11 22, 02 11 20
+
+# 숫자에서 2개씩 뽑는 경우의 수만큼 빙고 색칠해서 확인하는게 맞는듯
