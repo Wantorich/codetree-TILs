ee8a80c391169fed8b11877adccf0b78d264a9d8
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-05-05 18:44:43 +0900

    Title: 최대 최소간의 차, Time: 263ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240505/최대 최소간의 차/difference-between-maximum-and-minimum.py
+++ 240505/최대 최소간의 차/difference-between-maximum-and-minimum.py
@@ -1,18 +1,17 @@
 n, k = map(int, input().split())
 nums = list(map(int, input().split()))
 
-diff = max(nums) - min(nums)
 cost = 0
 idx = 0
 
 while max(nums) - min(nums) > k :
-    n = nums[idx]
-    if n == max(nums) :
+    num = nums[idx]
+    if num == max(nums) :
         nums[idx] -= 1
         cost += 1
-    elif n == min(nums) :
+    elif num == min(nums) :
         nums[idx] += 1
         cost += 1
-    idx = (idx+1) % n
+    idx = (idx+1) % n # 0 1 2 3 .. n-1 n->0
 
 print(cost)
