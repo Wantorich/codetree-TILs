76bd8ce123ee77e00be5feb6a3df6f74a3ae4829
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-05-05 18:57:42 +0900

    Title: 최대 최소간의 차, Time: 167ms, Memory: 26MB, Status: Passed - CodeTree

--- 240505/최대 최소간의 차/difference-between-maximum-and-minimum.py
+++ 240505/최대 최소간의 차/difference-between-maximum-and-minimum.py
@@ -3,15 +3,26 @@ nums = list(map(int, input().split()))
 
 cost = 0
 idx = 0
-
-while max(nums) - min(nums) > k :
-    num = nums[idx]
-    if num == max(nums) :
-        nums[idx] -= 1
-        cost += 1
-    elif num == min(nums) :
-        nums[idx] += 1
-        cost += 1
-    idx = (idx+1) % n # 0 1 2 3 .. n-1 n->0
+diff = max(nums) - min(nums)
+while diff > k :
+    max_val = max(nums)
+    min_val = min(nums)
+    min_idx = []
+    max_idx = []
+    for i in range(n) :
+        if nums[i] == max_val :
+            max_idx.append(i)
+        elif nums[i] == min_val :
+            min_idx.append(i)
+    
+    if len(max_idx) < len(min_idx) :
+        for index in max_idx :
+            nums[index] -= 1
+            cost += 1
+    else :
+        for index in min_idx :
+            nums[index] += 1
+            cost += 1
+    diff = max(nums) - min(nums)
 
 print(cost)
