af520d47617d04da95a1713aa0d27d09d8b564ab
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-05-13 12:29:02 +0900

    Title: 비둘기와 전기줄, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240513/비둘기와 전기줄/pigeons-and-electric-cords.py
@@ -0,0 +1,18 @@
+# 몇번의 비둘기가 2번이상 정보가 나오고
+# 위치가 바뀌어야 최소 옮긴 카운트 수가 올라감
+# N만큼 배열을 잡고 배열의 값을 바꿔주는걸로 판단
+
+n = int(input())
+arr = [-1] * (n+1)
+ans = 0
+
+for i in range(n) :
+    bird, place = map(int,input().split())
+    if arr[bird] == -1 : # 첫 정보
+        arr[bird] = place
+    else : # 이미 정보가 앞에서 주어진 경우
+        if arr[bird] != place :
+            ans += 1
+            arr[bird] = place
+
+print(ans)
