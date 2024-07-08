1fa5f85ce70f762dc281436cd8ce38f88284f031
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-07-08 22:26:54 +0900

    Title: ABC 줄 세우기, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240708/ABC 줄 세우기/abc-line-up.py
+++ 240708/ABC 줄 세우기/abc-line-up.py
@@ -1,20 +1,33 @@
 N = int(input())
 line = input().split()
 
+# cnt = 0
+# alpha = 'A'
+# while True:
+#     flag = True
+#     for i in range(N-1):
+#         if line[i] != alpha:
+#             flag = False
+#             # swap
+#             line[i], line[i+1] = line[i+1], line[i]
+#             # print(line)
+#             cnt += 1
+#         alpha = chr(ord(alpha)+1)
+#     alpha = 'A'
+#     if flag:
+#         break
+
+# print(cnt)
+
 cnt = 0
 alpha = 'A'
-while True:
-    flag = True
-    for i in range(N-1):
-        if line[i] != alpha:
-            flag = False
-            # swap
-            line[i], line[i+1] = line[i+1], line[i]
-            # print(line)
-            cnt += 1
-        alpha = chr(ord(alpha)+1) if alpha < 'Z' else 'A'
-    alpha = 'A'
-    if flag:
-        break
+# 끝부터 채운다
+for i in range(N):
+    alpha = chr(ord('A')+i)
+    target_idx = line.index(alpha)
+    line.insert(i, line[target_idx])
+    line.pop(target_idx+1)
+    cnt += target_idx - i
+    # print(''.join(line))
 
 print(cnt)
