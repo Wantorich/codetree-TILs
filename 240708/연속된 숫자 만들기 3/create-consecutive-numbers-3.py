179ca46067a3af601d23aca07c7cba7dc13566c6
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-07-08 21:59:02 +0900

    Title: 연속된 숫자 만들기 3, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240708/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
+++ 240708/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
@@ -1,15 +1,21 @@
 a, b, c = tuple(map(int, input().split()))
 
-cnt = 0
-while not ((a+1 == b) and (b+1 == c)):
+if a+1 == b and b+1 == c:
+    print(0)
+elif a+1 == b:
+    print(c-b-1)
+elif b+1 == c:
+    print(b-a-1)
+else:
     if b-a < c-b:
         t = b+1
-        a = b
-        b = t
+        print(c-t-1)
     else:
         t = b-1
-        c = b
-        b = t
-    cnt += 1
+        print(t-a-1)
 
-print(cnt)
+
+
+
+# 한번 a, a+1, b의 구조가 형성이 되면
+# 3 4 20되면 5~19까지 15번 c - b - 1 횟수
