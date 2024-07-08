9f77e423145d9cbec6305e61a97f3420a0c416d9
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-07-08 23:30:56 +0900

    Title: 독서실의 거리두기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240708/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 240708/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -7,16 +7,29 @@ seats = list(input())
 # 양끝이랑 비교 
 # 가장 넓은 공간에 넣음
 # 양끝이 0인경우 무조건 끝에 넣는게 이득임
+def cal_dis():
+    l = seats.index('1')
+    max_dis = N
+    for i in range(l+1, N):
+        if seats[i] == '1':
+            r = i
+            dis = r - l
+            max_dis = min(max_dis, dis)
+            l = r
+    return max_dis
 
-l_space = seats.index('1')
-# 0 0 0 1
-for i in range(N):
-    if seats[i] == '1':
-        last_idx = i
-r_space = N-1 - last_idx
-# 1 0 0 0 0
-# 0 1 1 1 0 0 -> 3
-# 0 0 1 1 1 0 -> 2
+result = [0] * 3
+
+if seats[0] == '0':
+    seats[0] = '1'
+    result[0] = cal_dis()
+    seats[0] = '0'
+
+if seats[-1] == '0':
+    seats[-1] = '1'
+    result[1] = cal_dis()
+    seats[-1] = '0'
+
 
 l = seats.index('1')
 max_space = 0
@@ -33,12 +46,11 @@ for i in range(seats.index('1')+1, N):
 
 insert_idx = (max_l+max_r)//2
 seats[insert_idx] = '1'
+result[2] = cal_dis()
+
 # print(''.join(seats))
 
-# print((max_l+max_r)//2 - max_l, l_space-1, r_space-1)
-result = max(insert_idx - max_l, l_space, r_space)
-print(result)
-
+print(max(result))
 
 # 한쪽만 1이있는경우와 양쪽에 1이 있는경우를 나누는경우
 # 전자는 무조건 끝에 배치하는게 이득
