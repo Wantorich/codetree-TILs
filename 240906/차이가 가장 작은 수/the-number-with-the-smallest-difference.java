ce9236a55a053b5f5ce965e7dff4278b59ed1304
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-09-06 16:21:54 +0900

    Title: 차이가 가장 작은 수, Time: 1271ms, Memory: 52MB, Status: Passed - CodeTree

--- 240906/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
+++ 240906/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
@@ -6,15 +6,21 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int M = sc.nextInt();
+        int [] nums = new int[N];
         TreeSet<Integer> ts = new TreeSet();
-        int minDis = Integer.MAX_VALUE;
         for (int i = 0; i < N; i++) {
             int num = sc.nextInt();
+            nums[i] = num;
             ts.add(num);
-            if (ts.higher(num) != null && ts.higher(num) - num >= M) minDis = Math.min(minDis, ts.higher(num) - num);
-            if (ts.lower(num) != null && num - ts.lower(num) >= M) minDis = Math.min(minDis, num - ts.lower(num));
         }
-        if (minDis == Integer.MAX_VALUE) minDis = -1;
-        System.out.println(minDis);
+
+        int minDiff = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int curr = nums[i];
+            if (ts.ceiling(curr + M) != null) minDiff = Math.min(minDiff, ts.ceiling(curr + M) - curr);
+            if (ts.floor(curr - M) != null) minDiff = Math.min(minDiff, curr - ts.floor(curr - M));
+        }
+        if (minDiff == Integer.MAX_VALUE) minDiff = -1;
+        System.out.println(minDiff);
     }
 }
