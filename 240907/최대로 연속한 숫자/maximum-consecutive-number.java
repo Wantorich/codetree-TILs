839f8ee7228187a236da11ea9a50af8846007ab6
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-09-07 10:34:32 +0900

    Title: 최대로 연속한 숫자, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240907/최대로 연속한 숫자/maximum-consecutive-number.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        TreeSet<Integer> delts = new TreeSet();
+        TreeSet<Integer> dists = new TreeSet();
+
+        for (int i = 0; i < M; i++) {
+            int del = sc.nextInt();
+            delts.add(del);
+            int left = -1, right = N+1;
+            int leftDis = 0, rightDis = 0;
+
+            if (delts.lower(del) != null) {
+                left = delts.lower(del);
+            }
+            if (delts.higher(del) != null) {
+                right = delts.higher(del);
+            }
+
+            leftDis = del - left - 1;
+            rightDis = right - del - 1;
+
+            dists.remove(leftDis + rightDis + 1);
+            dists.add(leftDis);
+            dists.add(rightDis);
+            System.out.println(dists.last());
+        }
+    }
+}
