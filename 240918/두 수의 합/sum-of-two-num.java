2d421fcc00f1ada1f54ac064c78b959c72e66594
hyojae <121792083+Wantorich@users.noreply.github.com>
2024-09-18 16:59:21 +0900

    Title: 두 수의 합, Time: 1865ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 240918/두 수의 합/sum-of-two-num.java
+++ 240918/두 수의 합/sum-of-two-num.java
@@ -5,9 +5,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int K = sc.nextInt();
-        Set<Integer> set = new HashSet();
-        while (N-- > 0) set.add(sc.nextInt());
-        Integer [] arr = set.toArray(new Integer[0]);
+        int [] arr = new int[N];
+        for (int i = 0; i < N; i++) arr[i] = sc.nextInt();
 
         Map<Integer, Integer> map = new HashMap();
         for (int i = 0; i < arr.length; i++) {
